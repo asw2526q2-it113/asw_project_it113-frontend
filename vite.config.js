2e07869ee83a81d00f47bf4d34d482702341de69
laiagesse <laia.gesse@estudiantat.upc.edu>
2026-05-20 10:53:15 +0200

    vite.config per qs

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,3 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
 
@@ -17,6 +14,12 @@ export default defineConfig({
 
   optimizeDeps: {
     include: ['qs'],
+  },
+
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
   },
 
   server: {
