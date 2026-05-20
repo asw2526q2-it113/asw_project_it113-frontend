715f23e04d3d1b6fb62d013bb124f2bed21c4c76
laiagesse <laia.gesse@estudiantat.upc.edu>
2026-05-20 10:45:00 +0200

    vite.config per qs

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,20 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      util: 'util/'
+    }
+  },
+
+  define: {
+    global: 'globalThis'
+  },
+
   server: {
     proxy: {
-      '/api': 'http://localhost:8000',  // ← redirigeix al Django
+      '/api': 'http://localhost:8000',
       '/media': 'http://localhost:8000',
     }
   }
